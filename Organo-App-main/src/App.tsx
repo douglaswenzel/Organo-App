ae0548edb002176018196e6b272ecb3b50a1e685
douglaswenzel <douglas.wenzel@hotmail.com>
2024-12-10 18:59:37 -0300

    Update Componentes

--- Organo-App-main/src/App.tsx
+++ Organo-App-main/src/App.tsx
@@ -3,12 +3,13 @@ import { v4 as id } from 'uuid';
 import Swal from 'sweetalert2';
 import { IUser } from "./shared/interfaces/iUser";
 import { ICrew } from "./shared/interfaces/iCrew";
-
+import Banner from "./shared/componentes/banner/banner";
 
 function App() {
 
   return (
     <div className="App">
+      <Banner></Banner>
       <h1>Olá mundo!</h1>
   
     </div>
