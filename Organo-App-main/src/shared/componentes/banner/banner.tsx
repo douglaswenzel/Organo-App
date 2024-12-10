64c8b5b7360742bbe24cced0cbcfa4af7be4f6d3
douglaswenzel <douglas.wenzel@hotmail.com>
2024-12-10 19:12:06 -0300

    Atualização componente Banner

--- Organo-App-main/src/shared/componentes/banner/banner.tsx
+++ Organo-App-main/src/shared/componentes/banner/banner.tsx
@@ -1,6 +1,10 @@
+import './banner.css'
+
 function Banner(){
     return(
+        <header className="banner">
         <img src="../../img/banner.png" alt="Banner" />
+        </header>
     )
 }
 
