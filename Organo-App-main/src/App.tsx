f75a5b6b58f99d16ca9e75ae6292c0b60520d1b1
douglaswenzel <douglas.wenzel@hotmail.com>
2024-12-10 19:19:25 -0300

    Update App

--- Organo-App-main/src/App.tsx
+++ Organo-App-main/src/App.tsx
@@ -1,8 +1,3 @@
-import { useState } from "react";
-import { v4 as id } from 'uuid';
-import Swal from 'sweetalert2';
-import { IUser } from "./shared/interfaces/iUser";
-import { ICrew } from "./shared/interfaces/iCrew";
 import Banner from "./shared/componentes/banner/banner";
 
 function App() {
